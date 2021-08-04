a3318c6687fc32bbec7406d2b8b3f56f06e3de0f
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-08-04 15:27:11 +0000

    pretty print todos

--- list_all.py
+++ list_all.py
@@ -5,7 +5,7 @@ import utilities
 def list_all():
     json_data = utilities.load_data()
     for todo in json_data:
-        print(todo)
+        print(f"ID: {todo['ID']}\nTODO: {todo['user_input']}\nTIME: {todo['time']}\n")
 
 
 list_all()
